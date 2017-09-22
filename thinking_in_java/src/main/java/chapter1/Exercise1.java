4928d05f086a7a91ecde79da62a2109bc2b9e9c7
ridanyuan <ridan.yuan@ele.me>
2017-09-22 10:58:31 +0800

    "add chapter1"

---
+++ thinking_in_java/src/main/java/chapter1/Exercise1.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+/**
+ * Created by yrd on 2017/9/22.
+ */
+public class Exercise1 {
+
+    public String Java() throws Exception {
+        System.out.println("Thinking in Java Exercise1");
+        return "Java";
+    }
+}
