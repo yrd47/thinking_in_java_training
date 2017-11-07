dbc207a6f31945ca373efbf7dfe18b234042bc6d
ridanyuan <ridan.yuan@ele.me>
2017-11-07 17:46:12 +0800

    add chapter14 example

---
+++ thinking_in_java_Code/src/main/java/chapter14/ToyTest.java
@@ -0,0 +1,54 @@
+package chapter14;
+
+/**
+ * Created by yrd on 2017/11/7.
+ */
+interface HasBatteries{}
+interface Waterproof{}
+interface Shoots{}
+
+class Toy {
+    Toy(){}
+    Toy(int i){}
+}
+
+class FancyToy extends Toy implements HasBatteries, Waterproof, Shoots {
+    FancyToy() {
+        super(1);
+    }
+}
+public class ToyTest {
+
+    static void printInfo(Class cc) {
+        System.out.println("Class name: " + cc.getName() +
+                " is interface? [" + cc.isInterface() + "]");
+        System.out.println("Simple name: " + cc.getSimpleName());
+        System.out.println("Canonical name: " + cc.getCanonicalName());
+    }
+
+    public static void main(String[] args) {
+        Class c =null;
+        try {
+            c = Class.forName("chapter14.FancyToy");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Can't find FancyToy");
+            System.exit(1);
+        }
+        printInfo(c);
+        for (Class face : c.getInterfaces()) {
+            printInfo(face);
+        }
+        Class up = c.getSuperclass();
+        Object object = null;
+        try {
+            object = up.newInstance();
+        } catch (InstantiationException e) {
+            System.out.println("Cannot instantiate");
+            System.exit(1);
+        } catch (IllegalAccessException e) {
+            System.out.println("Can't access");
+            System.exit(1);
+        }
+        printInfo(object.getClass());
+    }
+}
