4a88f6cbe9e5c74ddfbc9498450061cb87bc7bff
ridanyuan <ridan.yuan@ele.me>
2018-05-10 11:34:34 +0800

    temp commit

---
+++ note/chapter18.md
@@ -0,0 +1,7 @@
+# <center> 第18章 Java I/O系统
+## 18.1 File类
+File既代表一个特定的文件，也能代表一个目录下的一组文件的名称  
+## 18.2 输入和输出
+任何自InputStream或Reader派生而来的类都有reader()基本方法，用于读取单个字节或者字节数组  
+任何自OutputStream或Writer派生而来的类都有write()基本方法，用于写单个字节或者字节数组  
+
---
+++ thinking_in_java_Code/src/main/java/chapter18/c18_1/DirList.java
@@ -0,0 +1,39 @@
+package chapter18.c18_1;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+/**
+ * Created by yrd on 2017/11/16.
+ */
+
+class DirFilter implements FilenameFilter {
+    private Pattern pattern;
+
+    public DirFilter(String regex) {
+        pattern = Pattern.compile(regex);
+    }
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return pattern.matcher(name).matches();
+    }
+}
+
+public class DirList {
+    public static void main(String[] args) {
+        File path = new File(".");
+        String[] list;
+        if (args.length == 0) {
+            list = path.list();
+        } else {
+            list = path.list(new DirFilter(args[0]));
+        }
+        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for (String dirItem : list) {
+            System.out.println(dirItem);
+        }
+    }
+}
